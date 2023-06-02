b8b9d53fd02eb8998ddc4943089646b20190c5fa
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-06-02 13:39:50 +0100

    create custom button and create header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,9 @@ import { useRouter } from 'next/navigation';
 import React from 'react';
 import { twMerge } from 'tailwind-merge';
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
+import { HiHome } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi';
+import Button from './Button';
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
@@ -38,11 +41,40 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           </button>
         </div>
         <div className="flex md:hidden gap-x-2 items-center">
-          <button>
-            <HiHome />
+          <button
+            className=" rounded-full p-2 bg-white flex items-center hover:opacity-75
+           transition
+          "
+          >
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button
+            className=" rounded-full p-2 bg-white flex items-center hover:opacity-75
+           transition
+          "
+          >
+            <BiSearch className="text-black" size={20} />
           </button>
         </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                onClick={() => {}}
+                className=" bg-transparent text-neutral-300 font-medium"
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button onClick={() => {}} className=" bg-white px-6 py-2">
+                Login
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
